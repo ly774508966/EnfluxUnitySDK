a0644254068b1ac6e34bb7056713e51ed002b837
Jordan Tritell <jtritell@indiana.edu>
2017-02-28 11:01:32 -0800

    Launch Bluetooth manager from example UI
    Currently only supports Win10

---
+++ UnityProject/Assets/LaunchBTManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System;
+
+public class LaunchBTManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(Environment.OSVersion.Version.Major == 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LaunchBluetoothManager()
+    {
+        // Windows 10
+        Process cmd = new Process();
+
+        cmd.StartInfo.CreateNoWindow = true;
+        cmd.StartInfo.UseShellExecute = false;
+        cmd.StartInfo.FileName = "powershell.exe";
+        cmd.StartInfo.Arguments = "start ms-settings:bluetooth";
+        cmd.Start();
+
+        // Untested Windows 8 Command:
+        // control / name Microsoft.BluetoothDevices
+    }
+
+}
